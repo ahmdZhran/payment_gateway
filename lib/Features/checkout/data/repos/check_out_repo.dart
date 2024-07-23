f6ade338e8fc371e2c9cec6153e01bd5c28f5c45
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-24 00:48:10 +0300

    create serverFailure

--- lib/Features/checkout/data/repos/check_out_repo.dart
+++ lib/Features/checkout/data/repos/check_out_repo.dart
@@ -1,17 +1,10 @@
 import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent_input_model/payment_intent_input_mode.dart';
 import 'package:dartz/dartz.dart';
 
+import '../../../../core/errors/failure.dart';
+
 abstract class CheckoutRepo {
   Future<Either<Failure, void>> makePayment(
       {required PaymentIntentInputModel paaymentIntentInputModel});
 }
 
-abstract class Failure {
-  final String errMessage;
-
-  Failure({required this.errMessage});
-}
-
-class ServerFailure extends Failure {
-  ServerFailure({required super.errMessage});
-}
---
+++ lib/core/errors/server_failure.dart
@@ -0,0 +1,5 @@
+import 'package:checkout_payment_ui/core/errors/failure.dart';
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.errMessage});
+}
