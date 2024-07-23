9b3cbd8c04febe937495ba60eef396714955aad7
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-24 00:28:45 +0300

    create initpaymentSheet method

--- lib/core/services/payment/stripe_service.dart
+++ lib/core/services/payment/stripe_service.dart
@@ -2,6 +2,7 @@ import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent
 import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent_models/payment_intent_models.dart';
 import 'package:checkout_payment_ui/core/services/api/dio_service.dart';
 import 'package:checkout_payment_ui/core/utils/api_key.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 class StripeService {
   final ApiService apiService = ApiService();
@@ -14,5 +15,14 @@ class StripeService {
     );
     var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
     return paymentIntentModel;
+  }
+
+  Future initPaymentSheet({required String paymentIntentClientSecret}) async {
+    Stripe.instance.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+          paymentIntentClientSecret: paymentIntentClientSecret,
+          merchantDisplayName: 'Ahmed' // name of the buisness owner
+          ),
+    );
   }
 }
