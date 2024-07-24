42fb34d464ec2eb018fe594b32df0bb3c5565c6d
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-24 09:45:57 +0300

    create makepayment method in cubit and handle its states

--- lib/Features/checkout/logic/cubit/payment_cubit.dart
+++ lib/Features/checkout/logic/cubit/payment_cubit.dart
@@ -1,9 +1,25 @@
-
-
+import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent_input_model/payment_intent_input_mode.dart';
+import 'package:checkout_payment_ui/Features/checkout/data/repos/check_out_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'payment_state.dart';
 
 class PaymentCubit extends Cubit<PaymentState> {
-  PaymentCubit() : super(PaymentInitial());
+  PaymentCubit(this.checkoutRepo) : super(PaymentInitial());
+
+  final CheckoutRepo checkoutRepo;
+  Future makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    emit(PaymentLoading());
+
+    var data = await checkoutRepo.makePayment(
+        paaymentIntentInputModel: paymentIntentInputModel);
+
+    data.fold(
+      (l) => emit(PaymentFailure(l.errMessage)),
+      (r) => emit(
+        PaymentSuccess(),
+      ),
+    );
+  }
 }
