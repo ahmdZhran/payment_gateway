ac523ec435e9fce09312f05c916d1fd2cead32d3
Ahmed <ahmedmohameddx80@gmail.com>
2024-07-27 20:55:43 +0300

    add customerId

--- lib/Features/checkout/data/models/payment_intent_input_model/payment_intent_input_mode.dart
+++ lib/Features/checkout/data/models/payment_intent_input_model/payment_intent_input_mode.dart
@@ -1,13 +1,14 @@
 class PaymentIntentInputModel {
   final String amount;
   final String currency;
-
-  PaymentIntentInputModel({required this.amount, required this.currency});
+  final String customerId;
+  PaymentIntentInputModel(this.customerId, {required this.amount, required this.currency});
 
   toJson() {
     return {
       'amount': '${amount}00',
       'currency': currency,
+      'customer': customerId,
     };
   }
 }
